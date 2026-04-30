75ab5c265c0453648f4213a96ad5f931b5e84441
Kidus Abbay <kiabbay@ttu.edu>
2026-04-30 00:02:11 -0500

    lab07: done

--- lab07/Question3/student.c
+++ lab07/Question3/student.c
@@ -32,9 +32,35 @@ Notes:
 - The sorting should be done in ascending order.
 */
 
+int partition(int arr[], int low, int high) {
+    int pivot = arr[high]; 
+    int i = low - 1; 
+
+    for (int j = low; j < high; j++) {
+        if (arr[j] <= pivot) {
+            i++;
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+        }
+    }
+
+    int temp = arr[i+1];
+    arr[i+1] = arr[high];
+    arr[high] = temp;
+    return i + 1;
+}
+
+void quickSortHelper(int arr[], int low, int high) {
+    if (low >= high) return;
+
+    int pivotIndex = partition(arr, low, high);
+    quickSortHelper(arr, low, pivotIndex - 1);
+    quickSortHelper(arr, pivotIndex + 1, high);
+}
+
 void quickSort(int arr[], int size) {
-    // TODO: implement quick sort
-    (void)arr;
-    (void)size;
+    if (size <= 1) return;
+    quickSortHelper(arr, 0, size - 1);
 }
 
--- lab07/Question5/student.c
+++ lab07/Question5/student.c
@@ -22,7 +22,19 @@ Notes:
 
 void selectionSort(int arr[], int size) {
     // TODO: implement selection sort
-    (void)arr;
-    (void)size;
+    for (int i = 0; i < size - 1; i++) {
+        int minIdx = i;
+
+        for (int j = i + 1; j < size; j++) {
+            if (arr[j] < arr[minIdx])
+                minIdx = j;
+        }
+
+        if (minIdx != i) {
+            int temp  = arr[i];
+            arr[i]    = arr[minIdx];
+            arr[minIdx] = temp;
+        }
+    }
 }
 
